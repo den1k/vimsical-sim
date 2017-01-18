b4c876e43bd16e44c11c95c74939d3e339af3898
Julien Fantin <julienfantin@gmail.com>
2017-01-18 15:45:45 -0500

    Use aleph snapshot (fixes ws disconnection errors)

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :dependencies
   [[org.clojure/clojure       "1.8.0"]
    [org.clojure/core.async    "0.2.395"]
-   [aleph                     "0.4.1"]
+   [aleph                     "0.4.2-alpha12"]
    [cheshire                  "5.6.3"]
    [clj-gatling               "0.8.3"]
    [com.cognitect/transit-cljs"0.8.239"]
